681b0079810c5f101d2301c9103b62634099cb9b
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-17 09:21:34 +0200

    Feat: add project links + listeners to links

--- src/index.js
+++ src/index.js
@@ -3,12 +3,45 @@ import "./stylesheets/index.css";
 const gitIcon = document.querySelector(".gitIcon");
 const linkedInIcon = document.querySelector(".linkedInIcon");
 
-gitIcon.addEventListener(
-  "click",
-  () => (location.href = "https://github.com/Gohan61"),
-);
+const nahebookGit = document.querySelector(".nahebook-git");
+const nahebookLink = document.querySelector(".nahebook-link");
 
-linkedInIcon.addEventListener(
-  "click",
-  () => (location.href = "https://www.linkedin.com/in/gokhan-sancak/"),
-);
+const messageGit = document.querySelector(".message-git");
+const messageLink = document.querySelector(".message-link");
+
+const dataGit = document.querySelector(".data-git");
+const dataLink = document.querySelector(".data-link");
+
+const battleshipGit = document.querySelector(".battleship-git");
+const battleshipLink = document.querySelector(".battleship-link");
+
+const shoppingGit = document.querySelector(".shopping-git");
+const shoppingLink = document.querySelector(".shopping-link");
+
+const photoGit = document.querySelector(".photo-git");
+const photoLink = document.querySelector(".photo-link");
+
+function addLinkListener(link, element) {
+  element.addEventListener("click", () => (location.href = link));
+}
+
+(function addListeners() {
+  const linksAndElements = [
+    [gitIcon, "https://github.com/Gohan61"],
+    [linkedInIcon, "https://www.linkedin.com/in/gokhan-sancak/"],
+    [nahebookGit, "https://github.com/Gohan61/nahebook"],
+    [nahebookLink, "https://nahebook-odin-frontend.netlify.app/"],
+    [messageGit, "https://github.com/Gohan61/messaging-app"],
+    [messageLink, "https://messaging-app-odin-frontend.netlify.app/"],
+    [dataGit, "https://github.com/Gohan61/computer-science-odin"],
+    [dataLink, "https://github.com/Gohan61/computer-science-odin"],
+    [battleshipGit, "https://github.com/Gohan61/testing-javascript"],
+    [battleshipLink, "https://gohan61.github.io/testing-javascript/"],
+    [shoppingGit, "https://github.com/Gohan61/shopping-cart"],
+    [shoppingLink, "https://main--odin-react-shoppingcart.netlify.app/"],
+    [photoGit, "https://github.com/Gohan61/phototaggingapp"],
+    [photoLink, "https://production--phototag-frontend-odin.netlify.app/"],
+  ];
+
+  linksAndElements.forEach((item) => addLinkListener(item[1], item[0]));
+})();
