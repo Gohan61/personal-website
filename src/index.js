e49c98202c519fefc861a8330bbce77b58ef34a1
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-07-17 06:40:04 +0200

    Feat: link to socials + on hover pointer
    * Added unique classes to social Icons for easy querying
    * Both social icons lead to respective profiles
    * On hover over social icons cursor becomes hover to make it clear it is
    clickable

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./stylesheets/index.css";
+
+const gitIcon = document.querySelector(".gitIcon");
+const linkedInIcon = document.querySelector(".linkedInIcon");
+
+gitIcon.addEventListener(
+  "click",
+  () => (location.href = "https://github.com/Gohan61"),
+);
+
+linkedInIcon.addEventListener(
+  "click",
+  () => (location.href = "https://www.linkedin.com/in/gokhan-sancak/"),
+);
