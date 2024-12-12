d3c4e10ee3022ad59780429aa9a3f7121569df16
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-12 15:18:07 +0100

    Refactor: accessible links + open in new tab
    * Instead of clickable images, they are encapsuled in anchor elements to
    make it accessible (reading links they point to)
    * Links open in new tabs to make it easier to refer back to site

--- src/index.js
+++ src/index.js
@@ -11,10 +11,6 @@ import BstImage from "./assets/Binary_search_tree.svg";
 import FileUploaderImage from "./assets/file-uploader.png";
 import EmailIcon from "./assets/email-outline.svg";
 
-function addLinkListener(link, element) {
-  element.addEventListener("click", () => (location.href = link));
-}
-
 (function addListeners() {
   const gitIcon = document.querySelector(".gitIcon");
   const linkedInIcon = document.querySelector(".linkedInIcon");
@@ -23,10 +19,8 @@ function addLinkListener(link, element) {
   const nahebookLink = document.querySelector(".nahebook-link");
 
   const messageGit = document.querySelector(".message-git");
-  // const messageLink = document.querySelector(".message-link");
 
   const dataGit = document.querySelector(".data-git");
-  const dataLink = document.querySelector(".data-link");
 
   const battleshipGit = document.querySelector(".battleship-git");
   const battleshipLink = document.querySelector(".battleship-link");
@@ -35,27 +29,9 @@ function addLinkListener(link, element) {
   const shoppingLink = document.querySelector(".shopping-link");
 
   const fileUploadGit = document.querySelector(".photo-git");
-  // const photoLink = document.querySelector(".photo-link");
 
   const contactGit = document.querySelector(".contactIcons .gitIcon");
   const contactLinkedin = document.querySelector(".contactIcons .linkedInIcon");
-
-  const linksAndElements = [
-    [gitIcon, "https://github.com/Gohan61"],
-    [linkedInIcon, "https://www.linkedin.com/in/gokhan-sancak/"],
-    [nahebookGit, "https://github.com/Gohan61/nahebook"],
-    [nahebookLink, "https://nahebook-odin-frontend.netlify.app/"],
-    [messageGit, "https://github.com/Gohan61/messaging-app"],
-    [dataGit, "https://github.com/Gohan61/computer-science-odin"],
-    [dataLink, "https://github.com/Gohan61/computer-science-odin"],
-    [battleshipGit, "https://github.com/Gohan61/testing-javascript"],
-    [battleshipLink, "https://gohan61.github.io/testing-javascript/"],
-    [shoppingGit, "https://github.com/Gohan61/shopping-cart/tree/redux"],
-    [shoppingLink, "https://odin-react-shoppingcart.netlify.app/"],
-    [fileUploadGit, "https://github.com/Gohan61/file-uploader"],
-    [contactGit, "https://github.com/Gohan61"],
-    [contactLinkedin, "https://www.linkedin.com/in/gokhan-sancak/"],
-  ];
 
   gitIcon.src = GitIcon;
   nahebookGit.src = GitIcon;
@@ -70,13 +46,8 @@ function addLinkListener(link, element) {
   contactLinkedin.src = LinkedinIcon;
 
   nahebookLink.src = LinkIcon;
-  // messageLink.src = LinkIcon;
-  dataLink.src = LinkIcon;
   battleshipLink.src = LinkIcon;
   shoppingLink.src = LinkIcon;
-  // photoLink.src = LinkIcon;
-
-  linksAndElements.forEach((item) => addLinkListener(item[1], item[0]));
 
   const encEmail = "Zy5zYW5jYWtAcHJvdG9uLm1l";
   const email = document.querySelector(".email");
