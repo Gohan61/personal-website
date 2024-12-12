849a77dc3ad4a36bf2984f75b69fa3275f1f104a
Gohan61 <90203619+Gohan61@users.noreply.github.com>
2024-12-12 10:04:42 +0100

    Refactor: update links + images to live & repo
    * Messaging app does not have a live version anymore, thus removed
    * Shopping cart is pointing to different branch + updated image of new
    site
    * Phototag is replaced with other project

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import ProfilePicture from "./assets/Profile-picture-Gokhan.jpg";
 import NahebookImage from "./assets/Nahebook-desktop-feed (Phone).png";
 import MessageImage from "./assets/Messaging-app-display.png";
 import BattleshipImage from "./assets/battleship-display.png";
-import ShoppingImage from "./assets/Shopping-cart-display.png";
+import ShoppingImage from "./assets/fake-store-example.png";
 import BstImage from "./assets/Binary_search_tree.svg";
-import PhotoTagImage from "./assets/Phototag-display.png";
+import FileUploaderImage from "./assets/file-uploader.png";
 import EmailIcon from "./assets/email-outline.svg";
 
 function addLinkListener(link, element) {
@@ -23,7 +23,7 @@ function addLinkListener(link, element) {
   const nahebookLink = document.querySelector(".nahebook-link");
 
   const messageGit = document.querySelector(".message-git");
-  const messageLink = document.querySelector(".message-link");
+  // const messageLink = document.querySelector(".message-link");
 
   const dataGit = document.querySelector(".data-git");
   const dataLink = document.querySelector(".data-link");
@@ -34,8 +34,8 @@ function addLinkListener(link, element) {
   const shoppingGit = document.querySelector(".shopping-git");
   const shoppingLink = document.querySelector(".shopping-link");
 
-  const photoGit = document.querySelector(".photo-git");
-  const photoLink = document.querySelector(".photo-link");
+  const fileUploadGit = document.querySelector(".photo-git");
+  // const photoLink = document.querySelector(".photo-link");
 
   const contactGit = document.querySelector(".contactIcons .gitIcon");
   const contactLinkedin = document.querySelector(".contactIcons .linkedInIcon");
@@ -46,15 +46,15 @@ function addLinkListener(link, element) {
     [nahebookGit, "https://github.com/Gohan61/nahebook"],
     [nahebookLink, "https://nahebook-odin-frontend.netlify.app/"],
     [messageGit, "https://github.com/Gohan61/messaging-app"],
-    [messageLink, "https://messaging-app-odin-frontend.netlify.app/"],
+    // [messageLink, "https://github.com/Gohan61/messaging-app"],
     [dataGit, "https://github.com/Gohan61/computer-science-odin"],
     [dataLink, "https://github.com/Gohan61/computer-science-odin"],
     [battleshipGit, "https://github.com/Gohan61/testing-javascript"],
     [battleshipLink, "https://gohan61.github.io/testing-javascript/"],
-    [shoppingGit, "https://github.com/Gohan61/shopping-cart"],
-    [shoppingLink, "https://main--odin-react-shoppingcart.netlify.app/"],
-    [photoGit, "https://github.com/Gohan61/phototaggingapp"],
-    [photoLink, "https://production--phototag-frontend-odin.netlify.app/"],
+    [shoppingGit, "https://github.com/Gohan61/shopping-cart/tree/redux"],
+    [shoppingLink, "https://odin-react-shoppingcart.netlify.app/"],
+    [fileUploadGit, "https://github.com/Gohan61/file-uploader"],
+    // [photoLink, "https://production--phototag-frontend-odin.netlify.app/"],
     [contactGit, "https://github.com/Gohan61"],
     [contactLinkedin, "https://www.linkedin.com/in/gokhan-sancak/"],
   ];
@@ -62,22 +62,21 @@ function addLinkListener(link, element) {
   gitIcon.src = GitIcon;
   nahebookGit.src = GitIcon;
   messageGit.src = GitIcon;
-  messageGit.src = GitIcon;
   dataGit.src = GitIcon;
   battleshipGit.src = GitIcon;
   shoppingGit.src = GitIcon;
-  photoGit.src = GitIcon;
+  fileUploadGit.src = GitIcon;
   contactGit.src = GitIcon;
 
   linkedInIcon.src = LinkedinIcon;
   contactLinkedin.src = LinkedinIcon;
 
   nahebookLink.src = LinkIcon;
-  messageLink.src = LinkIcon;
+  // messageLink.src = LinkIcon;
   dataLink.src = LinkIcon;
   battleshipLink.src = LinkIcon;
   shoppingLink.src = LinkIcon;
-  photoLink.src = LinkIcon;
+  // photoLink.src = LinkIcon;
 
   linksAndElements.forEach((item) => addLinkListener(item[1], item[0]));
 
@@ -108,6 +107,6 @@ function addLinkListener(link, element) {
   const shoppingImage = document.querySelector(".project5 img");
   shoppingImage.src = ShoppingImage;
 
-  const photoTagImage = document.querySelector(".project6 img");
-  photoTagImage.src = PhotoTagImage;
+  const fileUploadImage = document.querySelector(".project6 img");
+  fileUploadImage.src = FileUploaderImage;
 })();
